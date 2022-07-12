2ce7d71c380ba837bfb8552ddd6fb394073c9ea2
devart <primary.devart@gmail.com>
2022-07-12 20:33:34 +0600

    layout ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,128 @@
+let valuesList = document.querySelectorAll('.dropdown-list'),
+	 valuesListItems = document.getElementsByClassName('dropdown-list__item'),
+	 valueToConvert = document.querySelectorAll('.current-value'),
+	 mainValueBlock = document.querySelector('.values');
+
+/*values animation*/
+let activeClasses = ["value-active", "basic__shadow"];
+
+	
+let inputs = document.querySelectorAll('.converter-box__input'),
+	inputInitial = inputs[0],
+	inputFinal = inputs[1];
+
+
+Object.entries(categories).forEach(([ name, item ]) => {
+	let elements = document.getElementsByClassName("value-item");
+	const el = document.createElement('div'); // value button wrapper
+	el.classList.add('value-item', 'basic__btn');
+	el.insertAdjacentHTML('beforeend', `
+	  <p class="value-item__title">${item.title}</p>
+		 <img src="assets/img/values/${name}.svg" alt="" class="svg-icon value-item__img">
+	`);
+	item.element = el;
+	el.addEventListener('click', () => changeValue(name, item));
+	//по этому name можно вытащить categories[name]
+	mainValueBlock.appendChild(el);
+	function changeValue(name, item) {
+		for (let i = 0; i < valueToConvert.length; i++) {
+			/* const valuesArray = [];
+			for (key in item.data) {
+				valuesArray.push(item.data[key].name);
+			}
+			valueToConvert[0].textContent = valuesArray[0];
+			valueToConvert[1].textContent = valuesArray[valueToConvert.length - 1]; */
+		};
+		
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].classList.remove(...activeClasses);
+		}
+		el.classList.add(...activeClasses);
+
+		while (valuesListItems.length != 0) {
+			i = 0;
+			valuesListItems[i].remove();
+			i++;
+		};;
+		
+		for (key in item.data) {
+			currentValueName = item.data[key].name;
+			currentValueStore = item.data[key].value;
+			console.log(key + ' it\'s key');
+			for (let i = 0; i < valuesList.length; i++) {
+				valuesList[i].insertAdjacentHTML('afterbegin', (`<li class="converter-box__list-item dropdown-list__item">${currentValueName}</li>`));
+				valueToConvert[0].textContent = currentValueName;
+				valueToConvert[1].textContent = currentValueName;
+			};
+
+			for (let i = 0; i < valuesList.length; i++) {
+				const currentValue = valueToConvert[i];
+				const currentList = valuesList[i];
+				const currentListItems = currentList.children;
+			
+				currentValue.addEventListener('mouseenter', function() {
+					currentList.classList.remove('hidden');
+				});
+			
+				currentValue.addEventListener('mouseleave', function() {
+					currentList.classList.add('hidden');
+				});
+				
+				currentList.addEventListener('mouseenter', function() {
+					currentList.classList.remove('hidden');
+				});
+			
+				currentList.addEventListener('mouseleave', function() {
+					currentList.classList.add('hidden');
+				});
+			
+				for (let j = 0; j < currentListItems.length; j++) {
+					const currentItem = currentListItems[j]
+			
+					currentItem.addEventListener('click', (e) => {
+						let text = currentItem.textContent;
+						currentValue.textContent = text;
+						currentList.classList.add('hidden');
+					});
+				};
+			};	//dropdown menu animation
+		}; //"for in" on clicked type of value
+		let converter = document.querySelector('.converter');
+		converter.onclick = function(event) {
+			let target = event.target;
+			if (target.tagName != "LI" || target.classList.contains('current-value')) {
+				return
+			} else {
+				currentValueStore = target.innerHTML;
+				console.log(currentValueStore);
+				console.log(categories.mass.data);
+			}
+		};
+
+	}; //event listener => change value function
+}); //global object "categories", .entries method
+
+//event delegation
+/* let selectedValue;
+mainValueBlock.onclick = function(event) {
+	let target = event.target;
+	let valueDiv = event.target.closest('div');
+	if (!valueDiv) return;
+	if (!mainValueBlock.contains(valueDiv)) return;
+	if (target.classList.contains('values')) return; */
+	/* highlight(valueDiv); */
+	/* changeValue(valueDiv); */
+/* }; */
+
+	//highlight current value button
+/* 	function highlight(valueBlock) {
+		selectedValue = valueBlock;
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].classList.remove(...activeClasses);
+		}
+		selectedValue.classList.add(...activeClasses);
+	} */
+
 /*svg to inline-svg*/
 $("img.svg-icon").each(function () {
 	let $img = $(this);
@@ -17,115 +142,17 @@ $("img.svg-icon").each(function () {
 });
 
 
-let valuesList = document.querySelectorAll('.dropdown-list'),
-	 valuesListItems = document.getElementsByClassName('dropdown-list__item'),
-	 valueToConvert = document.querySelectorAll('.current-value');
 
+let quoteDiv = document.getElementsByClassName('header__quote'),
+	 quoteTimer = setInterval(() => changeQuote(), 5000);
 
-/*values animation*/
-let elements = document.querySelectorAll(".value-item");
-let activeClasses = ["value-active", "basic__shadow"];
-
-let toggleActiveElement = element => {
-   element.addEventListener("click", () => {
-      elements.forEach(n => n.classList.remove(...activeClasses));
-      element.classList.add(...activeClasses);
-		for (key in mass) {
-			let massValues = mass[key];
-			let massValuesName = massValues.name;
-			
-			for (let j = 0; j < valuesListItems.length; j++) {
-				valuesListItems[j].remove();
-				console.log(1);
-			}
-		};
-
-		for (let i = 0; i < valuesList.length; i++) {
-			valuesList[i].insertAdjacentHTML('afterbegin', (`<li class="converter-box__list-item dropdown-list__item">${distance.cm.name}</li>`));
-		}
-   });
-};
-
-elements.forEach(v => toggleActiveElement(v));
-
-/*oninput TEST*/
-let inputs = document.querySelectorAll('.converter-box__input'),
-	inputInitial = inputs[0],
-	inputFinal = inputs[1];
-
-inputInitial.oninput = function() {
-	inputFinal.value = inputInitial.value * 2;
+function changeQuote() {
+	let randomQuote = Math.trunc(Math.random() * 100);
+	if (randomQuote > quotes.length - 1) {
+		return
+	} else {
+		quoteDiv[0].textContent = quotes[randomQuote];
+		quoteDiv[1].textContent = quotes[randomQuote];
+	}
 }
 
-inputFinal.oninput = function() {
-	inputInitial.value = inputFinal.value / 2;
-}
-
-/*values changing*/
-for (key in mass) {
-	let massValues = mass[key];
-	let massValuesName = massValues.name;
-	
-	for (let i = 0; i < valuesList.length; i++) {
-		valuesList[i].insertAdjacentHTML('afterbegin', (`<li class="converter-box__list-item dropdown-list__item">${massValuesName}</li>`));
-	}
-};
-
-function changingValues(btn, element) {
-	for (key in btn) {
-		let btnValues = btn[key];
-		let btnValuesName = massValues.name;
-		
-		for (let i = 0; i < valuesList.length; i++) {
-			valuesList[i].insertAdjacentHTML('afterbegin', (`<li class="converter-box__list-item dropdown-list__item">${massValuesName}</li>`));
-		}
-	};
-};
-
-/* for (key in distance) {
-	let distValues = distance[key];
-	let distValuesName = distValues.name;
-	
-	for (let i = 0; i < valuesList.length; i++) {
-		valuesList[i].insertAdjacentHTML('afterbegin', (`<li class="converter-box__list-item dropdown-list__item">${distValuesName}</li>`));
-	}
-};
- */
-
-/* for (let i = 0; i < valuesList.length; i++) {
-	valuesList[i].insertAdjacentHTML('afterbegin', (`<li class="converter-box__list-idropdown-list__item">${submainValues.name}</li>`));
-} */
-
-
-/*hiding dropdown*/
-for (let i = 0; i < valuesList.length; i++) {
-	const currentValue = valueToConvert[i];
-	const currentList = valuesList[i];
-	const currentListItems = currentList.children;
-
-	currentValue.addEventListener('mouseenter', function() {
-		currentList.classList.remove('hidden');
-	});
-
-	currentValue.addEventListener('mouseleave', function() {
-		currentList.classList.add('hidden');
-	});
-	
-	currentList.addEventListener('mouseenter', function() {
-		currentList.classList.remove('hidden');
-	});
-
-	currentList.addEventListener('mouseleave', function() {
-		currentList.classList.add('hidden');
-	});
-
-	for (let j = 0; j < currentListItems.length; j++) {
-		const currentItem = currentListItems[j]
-
-		currentItem.addEventListener('click', (e) => {
-			let text = currentItem.textContent
-			currentValue.textContent = text
-			currentList.classList.add('hidden');
-		});
-	};
-};
--- assets/js/valuesVar2.js
+++ assets/js/valuesVar2.js
@@ -1,191 +1,190 @@
-let mass = {
-	ton: {
-		name: "Тонна",
-		value: 1000
+const categories = {
+	mass: {
+		title: 'Масса',
+		element: null,
+		data: {
+			ton: {
+				name: "Тонна",
+				value: 1000
+				},
+			 centner: {
+				name: "Центнер",
+				value: 100
+				},
+			 kg: {
+				name: "Килограмм",
+				value: 1
+				},
+			 g: {
+				name: "Грамм",
+				value: 0.001
+				},
+			 mg: {
+				name: "Миллиграмм",
+				value: 0.000001
+				},
+			 carat: {
+				name: "Карат",
+				value: 20
+				}
 		},
-	centner: {
-		name: "Центнер",
-		value: 100
-		},
-	kg: {
-		name: "Килограмм",
-		value: 1
-		},
-	g: {
-		name: "Грамм",
-		value: 0.001
-		},
-	mg: {
-		name: "Миллиграмм",
-		value: 0.000001
-		},
-	carat: {
-		name: "Карат",
-		value: 20
-		}
-};
-
-let distance = {
-	km: {
-		name: "Километр",
-		value: 1
-		},
-	m: {
-		name: "Метр",
-		value: 0.1
-		},
-	cm: {
-		name: "Сантиметр",
-		value: 0.001
-		},
-	mm: {
-		name: "Миллиметр",
-		value: 0.0001
-		},
-	mile: {
-		name: "Миля",
-		value: 1.5
-		},
-	yard: {
-		name: "Ярд",
-		value: 20
-		},
-	foot: {
-		name: "Фут",
-		value: 30
-		},
-	inch: {
-		name: "Дюйм",
-		value: 2.5
-		}
-};
-	
-let temperature = {
-	km: {
-		name: "Километр",
-		value: 1
-		},
-	m: {
-		name: "Метр",
-		value: 0.1
-		},
-	cm: {
-		name: "Сантиметр",
-		value: 0.001
-		},
-	mm: {
-		name: "Миллиметр",
-		value: 0.0001
-		},
-	mile: {
-		name: "Миля",
-		value: 1.5
-		},
-	yard: {
-		name: "Ярд",
-		value: 20
-		},
-	foot: {
-		name: "Фут",
-		value: 30
-		},
-	inch: {
-		name: "Дюйм",
-		value: 2.5
-		}
-};
-
-let time = {
-	century: {
-		name: "Век",
-		value: ""
 	},
-	year: {
-		name: "Год",
-		value: ""
+	distance: {
+		title: 'Расстояние',
+		element: null,
+		data: {
+			km: {
+				name: "Километр",
+				value: 1
+				},
+			m: {
+				name: "Метр",
+				value: 0.1
+				},
+			cm: {
+				name: "Сантиметр",
+				value: 0.001
+				},
+			mm: {
+				name: "Миллиметр",
+				value: 0.0001
+				},
+			mile: {
+				name: "Миля",
+				value: 1.5
+				},
+			yard: {
+				name: "Ярд",
+				value: 20
+				},
+			foot: {
+				name: "Фут",
+				value: 30
+				},
+			inch: {
+				name: "Дюйм",
+				value: 2.5
+				}
+		},
 	},
-	month: {
-		name: "Месяц",
-		value: ""
+	temperature: {
+		title: 'Температура',
+		element: null,
+		data: {
+			c: {
+				name: "Цельсий",
+				value: 1
+				},
+			f: {
+				name: "Фаренгейт",
+				value: 0.1
+				},
+			k: {
+				name: "Кельвин",
+				value: 0.001
+				},
+		},
 	},
-	week: {
-		name: "Неделя",
-		value: ""
+	time: {
+		title: 'Время',
+		element: null,
+		data: {
+			century: {
+				name: "Век",
+				value: ""
+			},
+			year: {
+				name: "Год",
+				value: ""
+			},
+			month: {
+				name: "Месяц",
+				value: ""
+			},
+			week: {
+				name: "Неделя",
+				value: ""
+			},
+			day: {
+				name: "День",
+				value: ""
+			},
+			hour: {
+				name: "Час",
+				value: ""
+			},
+			minute: {
+				name: "Минута",
+				value: ""
+			},
+			second: {
+				name: "Секунда",
+				value: ""
+			},
+			millisecond: {
+				name: "Миллисекунда",
+				value: ""
+			},
+			edmTrack: {
+				name: "Средний EDM-трек",
+				value: "2.5"
+			}
+		},
 	},
-	day: {
-		name: "День",
-		value: ""
+	speed: {
+		title: 'Скорость',
+		element: null,
+		data: {
+			kmHour: {
+				name: "Километр в час",
+				value: ""
+			},
+			mileHour: {
+				name: "Миля в час",
+				value: ""
+			},
+			mSecond: {
+				name: "Метр в секунду",
+				value: ""
+			},
+			footSecond: {
+				name: "Фут в секунду",
+				value: ""
+			}
+		},
 	},
-	hour: {
-		name: "Час",
-		value: ""
+	currency: {
+		title: 'Валюта',
+		element: null,
+		data: {
+			KZT: {
+				name: "Тенге (KZT)",
+				value: ""
+			},
+			RUB: {
+				name: "Рубль (RUB)",
+				value: ""
+			},
+			USD: {
+				name: "Доллар США (USD)",
+				value: ""
+			},
+			EUR: {
+				name: "Евро (EUR)",
+				value: ""
+			},
+			GBP: {
+				name: "Фунт стерлингов (GBP)",
+				value: ""
+			},
+			BTC: {
+				name: "Биткоин (BTC)",
+				value: ""
+			},
+			ETH: {
+				name: "Эфир (ETH)",
+				value: ""
+			}
+		},
 	},
-	minute: {
-		name: "Минута",
-		value: ""
-	},
-	second: {
-		name: "Секунда",
-		value: ""
-	},
-	millisecond: {
-		name: "Миллисекунда",
-		value: ""
-	},
-	edmTrack: {
-		name: "Средний EDM-трек",
-		value: "2.5"
-	}
-};
-
-let speed = {
-	kmHour: {
-		name: "Километр в чат",
-		value: ""
-	},
-	mileHour: {
-		name: "Миля в час",
-		value: ""
-	},
-	mSecond: {
-		name: "Метр в секунду",
-		value: ""
-	},
-	footSecond: {
-		name: "Фут в секунду",
-		value: ""
-	}
-};
-
-let currency = {
-	KZT: {
-		name: "Тенге (KZT)",
-		value: ""
-	},
-	RUB: {
-		name: "Рубль (RUB)",
-		value: ""
-	},
-	USD: {
-		name: "Доллар США (USD)",
-		value: ""
-	},
-	EUR: {
-		name: "Евро (EUR)",
-		value: ""
-	},
-	GBP: {
-		name: "Фунт стерлингов (GBP)",
-		value: ""
-	},
-	BTC: {
-		name: "Биткоин (BTC)",
-		value: ""
-	},
-	ETH: {
-		name: "",
-		value: ""
-	}
-};
-
-let valueTypes = [mass, distance, temperature, time, speed, currency];
+ };
